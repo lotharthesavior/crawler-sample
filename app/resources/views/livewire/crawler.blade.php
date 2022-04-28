1c9ef6c3780405e27cb5749135eebec2d0a3d9b5
Savio Resende <savio@savioresende.com>
2022-04-28 00:44:45 -0600

    wip

--- app/resources/views/livewire/crawler.blade.php
+++ app/resources/views/livewire/crawler.blade.php
@@ -8,9 +8,9 @@
             <svg wire:loading width="45" height="45" viewBox="0 0 45 45" xmlns="http://www.w3.org/2000/svg" stroke="#000"><g fill="none" fill-rule="evenodd" transform="translate(1 1)" stroke-width="2"><circle cx="22" cy="22" r="6" stroke-opacity="0"><animate attributeName="r" begin="1.5s" dur="3s" values="6;22" calcMode="linear" repeatCount="indefinite" /><animate attributeName="stroke-opacity" begin="1.5s" dur="3s" values="1;0" calcMode="linear" repeatCount="indefinite" /><animate attributeName="stroke-width" begin="1.5s" dur="3s" values="2;0" calcMode="linear" repeatCount="indefinite" /></circle><circle cx="22" cy="22" r="6" stroke-opacity="0"><animate attributeName="r" begin="3s" dur="3s" values="6;22" calcMode="linear" repeatCount="indefinite" /><animate attributeName="stroke-opacity" begin="3s" dur="3s" values="1;0" calcMode="linear" repeatCount="indefinite" /><animate attributeName="stroke-width" begin="3s" dur="3s" values="2;0" calcMode="linear" repeatCount="indefinite" /></circle><circle cx="22" cy="22" r="8"><animate attributeName="r" begin="0s" dur="1.5s" values="6;1;2;3;4;5;6" calcMode="linear" repeatCount="indefinite" /></circle></g></svg>
         </div>
 
-        <hr x-show="crawled"/>
+        <hr x-show="crawled" x-cloak/>
 
-        <div x-show="crawled">
+        <div x-show="crawled" x-cloak>
             <div><strong class="mr-4">Number of pages crawled:</strong><span x-text="data.number_pages_crawled"></span></div>
             <div><strong class="mr-4">Number of a unique images:</strong><span x-text="data.number_unique_images"></span></div>
             <div><strong class="mr-4">Number of unique internal links:</strong><span x-text="data.number_unique_internal_links"></span></div>
@@ -20,9 +20,9 @@
             <div><strong class="mr-4">Average title length:</strong><span x-text="data.avg_title_length"></span></div>
         </div>
 
-        <hr x-show="crawled"/>
+        <hr x-show="crawled" x-cloak/>
 
-        <div x-show="crawled">
+        <div x-show="crawled" x-cloak>
             <div class="px-4 sm:px-6 lg:px-8">
                 <div class="mt-8 flex flex-col">
                     <div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
